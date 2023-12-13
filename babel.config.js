5aa7e873ad291bb8e3eb96ab755a5529ecf8334f
akshaysaambram <akshaysaambram@gmail.com>
2023-12-13 12:30:27 +0530

    Created a new Expo app

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
-
-  plugins.push('expo-router/babel');
 
   return {
     presets: ['babel-preset-expo'],
-    plugins,
+    plugins: ['expo-router/babel'],
   };
 };
